3dbd545015f7888034153dd25524c4776c7c5f36
Jason Ma <jasonma2010@yahoo.com.tw>
2021-06-27 17:08:47 +0800

    version 1

---
+++ XX.java
@@ -0,0 +1,26 @@
+package xxx;
+
+public class XX {
+	public static void main(String[] args) {
+//		String str = "Hello World";
+//		StringBuilder strb = new StringBuilder(str);
+//		str = strb.reverse().toString();
+//		System.out.println(str);
+		
+//		StringBuilder strb = new StringBuilder("Hello World");
+//		StringBuilder reverseStr = strb.reverse();
+//		System.out.println(reverseStr.toString());
+		
+		
+//		StringBuilder strb = new StringBuilder("Hello World");
+//		System.out.println(strb.reverse().toString());
+		
+		for(int i = 1; i<=9; i++) {
+			for(int j = 1; j<=9; j++) {
+				System.out.print(i +"x"+ j + "=" + i * j +" " );
+			}
+			System.out.println(ZZZZZZ);
+		}
+	}
+
+}	
